081fbaa1e8218d50963ede17ed5c6ca5a0c533be
Gal Kleinman <gal.klm@gmail.com>
2022-12-14 12:51:14 +0200

    remove create tenant change

--- apps/enrolla-server/src/tenants/create-tenant.dto.ts
+++ apps/enrolla-server/src/tenants/create-tenant.dto.ts
@@ -1,11 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
-import { IsInt } from 'class-validator';
 
 export class CreateTenantDto {
   @ApiProperty()
   organizationName: string;
   @ApiProperty()
-  @IsInt()
   githubInstallationId: number;
 }
