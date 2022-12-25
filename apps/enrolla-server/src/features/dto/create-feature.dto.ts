f6c7585bd507f2064535b2d34bf205f6726976bb
Gal Kleinman <gal.klm@gmail.com>
2022-12-25 12:41:26 +0200

    add dto validation

--- apps/enrolla-server/src/features/dto/create-feature.dto.ts
+++ apps/enrolla-server/src/features/dto/create-feature.dto.ts
@@ -1,8 +1,24 @@
-import { FeatureType } from "@prisma/client";
+import { ApiProperty } from '@nestjs/swagger';
+import { FeatureType } from '@prisma/client';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class CreateFeatureDto {
+  @ApiProperty()
+  @IsString()
   tenantId: string;
+
+  @ApiProperty()
+  @IsString()
   key: string;
+
+  @ApiProperty({
+    enum: FeatureType,
+  })
+  @IsEnum(FeatureType)
   type: FeatureType;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
   description?: string;
 }
