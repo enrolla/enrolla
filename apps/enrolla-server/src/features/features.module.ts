0730aafa93a1ecb327073f210e1f7ea01f7842bd
Gal Kleinman <gal.klm@gmail.com>
2022-12-25 11:40:00 +0200

    add prismaservice provider

--- apps/enrolla-server/src/features/features.module.ts
+++ apps/enrolla-server/src/features/features.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FeaturesService } from './features.service';
 import { FeaturesController } from './features.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [FeaturesController],
-  providers: [FeaturesService]
+  providers: [FeaturesService, PrismaService]
 })
 export class FeaturesModule {}
