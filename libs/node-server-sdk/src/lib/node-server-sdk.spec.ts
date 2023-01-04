311356d3315f7cc8ca0c97ea0ed0c492f862d160
Tomer <4tomer.friedman@gmail.com>
2023-01-04 16:12:07 +0200

    generate lib

---
+++ libs/node-server-sdk/src/lib/node-server-sdk.spec.ts
@@ -0,0 +1,7 @@
+import { nodeServerSdk } from './node-server-sdk';
+
+describe('nodeServerSdk', () => {
+  it('should work', () => {
+    expect(nodeServerSdk()).toEqual('node-server-sdk');
+  });
+});
