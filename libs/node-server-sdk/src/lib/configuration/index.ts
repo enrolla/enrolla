69b8c00d780102b74f61e880cbade955c3b4f72b
Tomer <4tomer.friedman@gmail.com>
2023-01-09 19:35:50 +0200

    exports

--- libs/node-server-sdk/src/lib/configuration/index.ts
+++ libs/node-server-sdk/src/lib/configuration/index.ts
@@ -3,7 +3,7 @@ import { InitialzeOptions } from '../interfaces';
 
 export let _configuration: InitialzeOptions;
 
-export const init = (options: InitialzeOptions) => {
+export const initialize = (options: InitialzeOptions) => {
   _configuration = Object.freeze(options);
 
   initializeGraphQLClient(_configuration);
