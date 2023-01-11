8a6f15a1bcce06c0dd7a3fff9906f1f05c856d84
Tomer <4tomer.friedman@gmail.com>
2023-01-11 18:51:56 +0200

    ui

---
+++ apps/mgmt-ui/src/pages/api-tokens/list.tsx
@@ -0,0 +1,164 @@
+import { useTable, ColumnDef, flexRender } from '@pankod/refine-react-table';
+import {
+  Box,
+  Text,
+  Group,
+  List,
+  ScrollArea,
+  Table,
+  Pagination,
+  DeleteButton,
+  DateField,
+  CopyButton,
+  Button,
+  Tooltip,
+  ActionIcon,
+} from '@pankod/refine-mantine';
+import { Flex } from '@mantine/core';
+import { IApiToken } from '../../interfaces';
+import { useMemo } from 'react';
+import { IconCopy, IconCheck } from '@tabler/icons';
+
+export const ApiTokenList: React.FC = () => {
+  const columns = useMemo<ColumnDef<IApiToken>[]>(
+    () => [
+      {
+        id: 'name',
+        header: 'Name',
+        accessorKey: 'name',
+      },
+      {
+        id: 'token',
+        header: 'Token Value',
+        accessorKey: 'token',
+        cell: function render({ getValue }) {
+          return (
+            <Flex
+              style={{ width: 200 }}
+              justify="flex-start"
+              align="flex-start"
+              direction="row"
+            >
+              <Text truncate>{getValue() as string}</Text>
+              <CopyButton value={getValue() as string} timeout={2000}>
+                {({ copied, copy }) => (
+                  <Tooltip
+                    label={copied ? 'Copied' : 'Copy'}
+                    withArrow
+                    position="right"
+                  >
+                    <ActionIcon color={copied ? 'teal' : 'gray'} onClick={copy}>
+                      {copied ? (
+                        <IconCheck size={16} />
+                      ) : (
+                        <IconCopy size={16} />
+                      )}
+                    </ActionIcon>
+                  </Tooltip>
+                )}
+              </CopyButton>
+            </Flex>
+          );
+        },
+      },
+      {
+        id: 'createdAt',
+        header: 'Created At',
+        accessorKey: 'createdAt',
+        cell: function render({ getValue }) {
+          return <DateField value={getValue() as string} format="LLL" />;
+        },
+      },
+      {
+        id: 'actions',
+        header: 'Actions',
+        accessorKey: 'id',
+        enableColumnFilter: false,
+        enableSorting: false,
+        cell: function render({ getValue }) {
+          return (
+            <Group spacing="xs" noWrap>
+              <DeleteButton
+                hideText
+                recordItemId={getValue() as number}
+                confirmTitle="This action cannot be undone! Make sure your API Token is not is use in your application before deleting it."
+                confirmOkText="I'm sure, delete it."
+                confirmCancelText="Nevermind, don't delete."
+              />
+            </Group>
+          );
+        },
+      },
+    ],
+    []
+  );
+
+  const {
+    getHeaderGroups,
+    getRowModel,
+    refineCore: { setCurrent, pageCount, current },
+  } = useTable({
+    columns,
+    refineCoreProps: {
+      metaData: {
+        fields: ['id', 'name', 'token', 'createdAt'],
+      },
+    },
+  });
+
+  return (
+    <ScrollArea>
+      <List>
+        <Table highlightOnHover>
+          <thead>
+            {getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th key={header.id}>
+                      {!header.isPlaceholder && (
+                        <Group spacing="xs" noWrap>
+                          <Box>
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                          </Box>
+                        </Group>
+                      )}
+                    </th>
+                  );
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {getRowModel().rows.map((row) => {
+              return (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+        <br />
+        <Pagination
+          position="right"
+          total={pageCount}
+          page={current}
+          onChange={setCurrent}
+        />
+      </List>
+    </ScrollArea>
+  );
+};
