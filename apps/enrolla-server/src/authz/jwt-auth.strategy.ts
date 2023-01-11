184c7703382c506223c5d20cf3ff17b62d355458
Gal Kleinman <gal.klm@gmail.com>
2023-01-11 14:44:26 +0200

    adding comments

--- apps/enrolla-server/src/authz/jwt-auth.strategy.ts
+++ apps/enrolla-server/src/authz/jwt-auth.strategy.ts
@@ -19,11 +19,14 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'JwtAuth') {
     const jwt = payload.headers['authorization'];
 
     try {
+      // Trying to validate the JWT as a PropelAuth token by default, representing management ui access
       const { userId, orgIdToOrgMemberInfo: tenantId } =
         await this.propelAuth.validateAccessTokenAndGetUser(jwt);
 
       return { userId, tenantId, isSdk: false };
     } catch (error) {
+      // If the JWT is not a PropelAuth token, we assume it's an API token, representing SDK access
+      // In case of an error, the method will throw an exception and 401 unauthorized will be returned
       const { tenantId } = await this.tenantsService.validateApiToken(
         jwt.split(' ')[1]
       );
