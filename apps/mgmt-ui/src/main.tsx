88f75628e1f29d34a9310e61078b85796ea3819d
Nir Gazit <nir.gzt@gmail.com>
2023-01-18 15:42:51 -0800

    chore: added posthog

--- apps/mgmt-ui/src/main.tsx
+++ apps/mgmt-ui/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
+import posthog from 'posthog-js';
+
 import { AuthProvider } from '@propelauth/react';
 
 import App from './App';
@@ -14,6 +16,16 @@ const monkeyPatchPropelAuthUrl = () => {
     return import.meta.env.VITE_PROPELAUTH_URL;
   }
 };
+
+if (
+  !window.location.host.includes('127.0.0.1') &&
+  !window.location.host.includes('localhost') &&
+  !window.location.host.includes('staging')
+) {
+  posthog.init(import.meta.env.VITE_POSTHOG_TOKEN, {
+    api_host: 'https://app.posthog.com',
+  });
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
