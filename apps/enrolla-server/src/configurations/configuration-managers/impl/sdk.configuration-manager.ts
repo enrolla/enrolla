d736da012b68ce77c25fdbbb08e4a0a55e2d68bf
Tomer <4tomer.friedman@gmail.com>
2023-01-19 18:08:39 +0200

    cleanup

--- apps/enrolla-server/src/configurations/configuration-managers/impl/sdk.configuration-manager.ts
+++ apps/enrolla-server/src/configurations/configuration-managers/impl/sdk.configuration-manager.ts
@@ -7,26 +7,23 @@ import { Injectable, Logger } from '@nestjs/common';
 export class SdkConfigurationManager implements ConfigurationManager {
   private readonly logger = new Logger(SdkConfigurationManager.name);
 
-  DEFAULT_POLLING_INTERVAL_SECONDS = 10;
-  private url = env.SDK_ENROLLA_SERVER_GRAPHQL_ENDPOINT;
-  private apiToken = env.SDK_API_TOKEN;
-  private privateKey = env.SDK_ENROLLA_PRIVATE_ENCRYPTION_KEY;
-  private pollingEnabled = env.SDK_POLLING_ENABLED
-    ? env.SDK_POLLING_ENABLED === 'true'
+  private readonly pollingEnabled = env.SDK_POLLING_ENABLED
+    ? env.SDK_POLLING_ENABLED === true.toString()
     : true;
-  private pollingIntervalSeconds = env.SDK_POLLING_INTERVAL_SECONDS
-    ? parseInt(env.SDK_POLLING_INTERVAL_SECONDS)
-    : this.DEFAULT_POLLING_INTERVAL_SECONDS;
+  private readonly DEFAULT_POLLING_INTERVAL_SECONDS = 60;
+  private pollingIntervalSeconds =
+    Number(env.SDK_POLLING_INTERVAL_SECONDS) ??
+    this.DEFAULT_POLLING_INTERVAL_SECONDS;
 
   constructor() {
     sdk
       .initialize({
-        url: this.url,
-        apiToken: this.apiToken,
-        privateKey: this.privateKey,
+        url: env.SDK_ENROLLA_SERVER_GRAPHQL_ENDPOINT,
+        apiToken: env.SDK_API_TOKEN,
+        privateKey: env.SDK_ENROLLA_PRIVATE_ENCRYPTION_KEY,
         polling: {
           enabled: this.pollingEnabled,
-          intervalSeconds: 10,
+          intervalSeconds: this.pollingIntervalSeconds ?? 60,
           onError: (error) => this.logger.error('onPollingError', error),
         },
       })
