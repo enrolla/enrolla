fd9060137da18ebb193e770877cd3e2b8b0d4ba5
Tomer <4tomer.friedman@gmail.com>
2023-01-23 18:19:28 +0200

    pretty

--- apps/enrolla-server/src/tracer.ts
+++ apps/enrolla-server/src/tracer.ts
@@ -3,17 +3,20 @@ import axios from 'axios';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-isProd && tracer.init({
-  profiling: true,
-  env: 'staging', //todo: get from env or inject in build file
-  service: 'enrolla-server',
-}); // initialized in a different file to avoid hoisting.
+isProd &&
+  tracer.init({
+    profiling: true,
+    env: 'staging', //todo: get from env or inject in build file
+    service: 'enrolla-server',
+  }); // initialized in a different file to avoid hoisting.
 
 // @see: https://docs.datadoghq.com/containers/amazon_ecs/apm/?tab=ec2metadataendpoint&code-lang=nodejs#code
-(async () => { 
+(async () => {
   if (!isProd) return;
 
-  const { data: hostname } = await axios.get('http://169.254.169.254/latest/meta-data/local-ipv4');
+  const { data: hostname } = await axios.get(
+    'http://169.254.169.254/latest/meta-data/local-ipv4'
+  );
   tracer.setUrl(`http://${hostname}:8126`);
 })();
 
