e239ee1386b0567d6927edd673c124346748a156
Tomer <42910634+tomer-friedman@users.noreply.github.com>
2023-01-30 17:57:49 +0200

    feat(docs) Core Concepts (#121)

--- libs/node-server-sdk/README.md
+++ libs/node-server-sdk/README.md
@@ -1,5 +1,9 @@
 # node-server-sdk
 
+[![jsDocs.io](https://img.shields.io/badge/jsDocs.io-reference-blue)](https://www.jsdocs.io/package/@enrolla/node-server-sdk)
+
 Enrolla SDK for NodeJS.
 
 See the [Full Documentation](https://docs.enrolla.io) for more details.
+
+Specifically, see the [NodeJS SDK Docs](https://docs.enrolla.io/sdk/node).
--- libs/node-server-sdk/src/lib/mgmt-api/customers.ts
+++ libs/node-server-sdk/src/lib/mgmt-api/customers.ts
@@ -20,7 +20,9 @@ export const createCustomer = async (input: {
   organizationId?: string;
   createOrganizationName?: string;
 }) => {
-  return await api.createCustomer(input);
+  const { createCustomer } = await api.createCustomer(input);
+
+  return createCustomer as Customer;
 };
 
 /**
@@ -51,7 +53,7 @@ export const updateCustomerSecrets = async (
 
   setCustomerSecrets(updateCustomerByOrgId as Partial<Customer>);
 
-  return updateCustomerByOrgId;
+  return updateCustomerByOrgId as Customer;
 };
 
 /**
@@ -78,5 +80,5 @@ export const updateCustomerFeatures = async (
 
   setCustomerFeatures(updateCustomerByOrgId as Partial<Customer>);
 
-  return updateCustomerByOrgId;
+  return updateCustomerByOrgId as Customer;
 };
