e34c6c4bc2370903dcecc8fdb807b3b6b5774a59
Tomer <4tomer.friedman@gmail.com>
2023-02-07 08:56:43 +0200

    naive implementation

--- apps/enrolla-server/src/pubsub/pubsub.service.ts
+++ apps/enrolla-server/src/pubsub/pubsub.service.ts
@@ -1,12 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PubSub } from 'graphql-subscriptions';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import { Redis } from 'ioredis';
 
 @Injectable()
 export class PubSubService {
-  private _pubSub: PubSub;
+  private _pubSub: PubSub | RedisPubSub;
+  private logger = new Logger(PubSubService.name);
 
   constructor() {
-    this._pubSub = new PubSub();
+    const url = process.env.REDIS_URL;
+    if (url) {
+      this.logger.log('PubSub Redis URL supplied, using RedisPubSub - ', url);
+
+      this._pubSub = new RedisPubSub({
+        publisher: new Redis(url),
+        subscriber: new Redis(url),
+      });
+    } else {
+      this.logger.log('No PubSub Redis URL supplied, using in memory PubSub');
+      this._pubSub = new PubSub();
+    }
   }
 
   public get pubSub() {
