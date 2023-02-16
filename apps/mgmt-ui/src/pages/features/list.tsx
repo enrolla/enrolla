c4293a301ed0561f3fb1e295a6844d2a50d9b95e
Tomer <4tomer.friedman@gmail.com>
2023-02-16 14:11:53 -0800

    console traces

--- apps/mgmt-ui/src/pages/features/list.tsx
+++ apps/mgmt-ui/src/pages/features/list.tsx
@@ -1,77 +1,167 @@
-import { useTable, ColumnDef, flexRender } from '@pankod/refine-react-table';
+import 'regenerator-runtime/runtime';
+import {
+  useTable,
+  useSortBy,
+  useGlobalFilter,
+  useAsyncDebounce,
+} from 'react-table';
+
 import {
   Box,
   Group,
   List,
   ScrollArea,
   Table,
-  Pagination,
   ShowButton,
   DeleteButton,
   DateField,
   Text,
   Space,
+  Center,
+  createStyles,
+  TextInput,
 } from '@pankod/refine-mantine';
 import { Feature, FeatureType } from '@enrolla/graphql-codegen';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import {
   FeatureViewComponent,
   FEATURE_TYPE_NAMES,
 } from '../../components/features/FeatureViewComponent';
-import { useNavigation } from '@pankod/refine-core';
+import { useNavigation, useList } from '@pankod/refine-core';
 import { FeaturesEmptyStateComponent } from '../../components/empty-state/EmptyStateComponent';
+import { IconSelector, IconChevronDown, IconChevronUp, IconSearch } from '@tabler/icons';
+
+
+const useStyles = createStyles((theme) => ({
+  title: {
+    color: theme.colorScheme === 'dark' ? '#FFFFFF' : '#38404E',
+    fontSize: 60,
+    fontWeight: 800,
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 42,
+    },
+    margin: '80px 0 0 50px',
+  },
+
+  description: {
+    color: theme.colorScheme === 'dark' ? '#A9ACB7' : '#38404E',
+    maxWidth: '500px',
+    margin: '25px 0 0 50px',
+  },
+
+  searchBar: {
+    width: '60%',
+    margin: '20px 0 0 50px',
+  },
+
+  table: {
+    width: '90%',
+    margin: '20px 0 0 50px',
+  },
+
+  button: {
+    background: '#9376EF',
+    margin: '30px 0 0 50px',
+    minWidth: '190px',
+    fontWeight: 100,
+
+    '&:hover': {
+      boxShadow: theme.shadows.sm,
+      background: '#9376EF',
+    },
+  },
+
+  th: {
+    padding: '0 !important',
+  },
+
+  control: {
+    width: '100%',
+    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    },
+  },
+
+  icon: {
+    width: 21,
+    height: 21,
+    borderRadius: 21,
+  },
+}));
+  
+// Define a default UI for filtering
+function GlobalFilter({
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const { classes } = useStyles();
+
+  const [value, setValue] = useState(globalFilter);
+  const onChange = useAsyncDebounce((value) => {
+    setGlobalFilter(value || undefined);
+  }, 200);
+
+  return (
+      <TextInput
+      className={classes.searchBar}
+        value={value || ''}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        icon={<IconSearch size={14} stroke={1.5} />}
+        placeholder="Search Features"
+      />
+  );
+}
 
 export const FeatureList: React.FC = () => {
-  const columns = useMemo<ColumnDef<Feature>[]>(
+  const columns = useMemo(
     () => [
       {
-        id: 'key',
-        header: 'Key',
-        accessorKey: 'key',
+        Header: 'Key',
+        accessor: 'key',
       },
       {
-        id: 'type',
-        header: 'Type',
-        accessorKey: 'type',
-        cell: ({ getValue }) => {
-          return FEATURE_TYPE_NAMES[getValue() as FeatureType];
+        Header: 'Type',
+        accessor: 'type',
+        Cell: ({ value }) => {
+          return FEATURE_TYPE_NAMES[value as FeatureType];
         },
       },
       {
-        id: 'defaultValue',
-        header: 'Default Value',
-        accessorKey: 'defaultValue',
-        cell: ({ getValue, row }) => (
+        Header: 'Default Value',
+        accessor: 'defaultValue',
+        Cell: ({ value, row }) => (
           <FeatureViewComponent
-            value={(getValue() as Feature['defaultValue']).value}
-            type={row.getValue('type') as FeatureType}
+            value={(value as Feature['defaultValue']).value}
+            type={row.value as FeatureType}
           />
         ),
       },
       {
-        id: 'description',
-        header: 'Description',
-        accessorKey: 'description',
+        Header: 'Description',
+        accessor: 'description',
       },
       {
-        id: 'createdAt',
-        header: 'Created At',
-        accessorKey: 'createdAt',
-        cell: function render({ getValue }) {
-          return <DateField value={getValue() as string} format="LLL" />;
+        Header: 'Created At',
+        accessor: 'createdAt',
+        sortBy: 'datetime',
+        Cell: function render({ value }) {
+          return <DateField value={value as string} format="LLL" />;
         },
       },
       {
         id: 'actions',
-        header: 'Actions',
-        accessorKey: 'id',
-        enableColumnFilter: false,
-        enableSorting: false,
-        cell: function render({ getValue }) {
+        Header: 'Actions',
+        accessor: 'id',
+        Cell: function render({ value }) {
           return (
             <Group spacing="xs" noWrap>
-              <ShowButton hideText recordItemId={getValue() as number} />
-              <DeleteButton hideText recordItemId={getValue() as number} />
+              <ShowButton hideText recordItemId={value as number} />
+              <DeleteButton hideText recordItemId={value as number} />
             </Group>
           );
         },
@@ -80,41 +170,55 @@ export const FeatureList: React.FC = () => {
     []
   );
 
-  const {
-    getHeaderGroups,
-    getRowModel,
-    refineCore: { setCurrent, pageCount, current, tableQueryResult },
-  } = useTable({
-    columns,
-    refineCoreProps: {
-      metaData: {
-        fields: [
-          'id',
-          'key',
-          'type',
-          'defaultValue',
-          'description',
-          'createdAt',
-        ],
-      },
+  const { data: featureList } = useList<Feature>({
+    resource: 'features',
+    metaData: {
+      fields: ['id', 'key', 'type', 'defaultValue', 'description', 'createdAt'],
     },
   });
 
+  const data = useMemo(() => featureList?.data || [], [featureList]);
+
+  // console.log('data', data);
+
+  //   if (!featureList?.data?.length) {
+  //   return <FeaturesEmptyStateComponent />;
+  // }
+
+  const {
+    getTableProps,
+    prepareRow,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    state,
+    visibleColumns,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable<Feature>(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter,
+    useSortBy,
+
+  );
+
   const { list } = useNavigation();
 
-  if (!tableQueryResult.isLoading && !getRowModel().rows.length) {
-    return <FeaturesEmptyStateComponent />;
-  }
+  const { classes } = useStyles();
+
+  // if (!tableQueryResult.isLoading && !getRowModel().rows.length) {
+  //   return <FeaturesEmptyStateComponent />;
+  // }
 
   return (
     <ScrollArea>
-      <List>
-        <Text>
+      <List title='lala'>
+        <Text className={classes.description}>
           Manage all of your features by assigning them types and default
-          values.
-        </Text>
-        <Text>
-          Group together sets of features with specific defaults into{' '}
+          values. Group together sets of features with specific defaults into{' '}
           <Text
             span
             c="blue"
@@ -137,41 +241,42 @@ export const FeatureList: React.FC = () => {
           .
         </Text>
         <Space h="md" />
-        <Table highlightOnHover>
+        <GlobalFilter
+          globalFilter={state.globalFilter}
+          setGlobalFilter={setGlobalFilter}
+        />
+        <Table className={classes.table} highlightOnHover {...getTableProps()}>
           <thead>
-            {getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+                {headerGroup.headers.map((column) => {
+                    const Icon = column.isSorted ? (column.isSortedDesc ? IconChevronUp : IconChevronDown) : IconSelector;
+
                   return (
-                    <th key={header.id}>
-                      {!header.isPlaceholder && (
-                        <Group spacing="xs" noWrap>
-                          <Box>
-                            {flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                          </Box>
-                        </Group>
-                      )}
+                    <th
+                      {...column.getHeaderProps(column.getSortByToggleProps())}
+                      key={column.id}
+                    >
+                      <Group spacing="xs" noWrap>
+                        <Box>{column.render('Header')}</Box>
+                        <Center className={classes.icon}>
+                        <Icon size={14} stroke={1.5} />
+                        </Center>
+                      </Group>
                     </th>
                   );
                 })}
               </tr>
             ))}
           </thead>
-          <tbody>
-            {getRowModel().rows.map((row) => {
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
               return (
-                <tr key={row.id}>
-                  {row.getVisibleCells().map((cell) => {
+                <tr {...row.getRowProps()} key={row.id}>
+                  {row.cells.map((cell) => {
                     return (
-                      <td key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </td>
+                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
                     );
                   })}
                 </tr>
@@ -180,12 +285,6 @@ export const FeatureList: React.FC = () => {
           </tbody>
         </Table>
         <br />
-        <Pagination
-          position="right"
-          total={pageCount}
-          page={current}
-          onChange={setCurrent}
-        />
       </List>
     </ScrollArea>
   );
